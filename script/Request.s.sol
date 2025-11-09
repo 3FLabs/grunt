62f8ededb62c9ba8937a07d01d2076f8635147a3
Maxime Brugel <maxime.brugel@kiln.fi>
2025-11-09 22:01:05 +0100

    feat: add request contract

--- script/Request.s.sol
+++ script/Request.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Request} from "../src/request/Request.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract RequestScript is Script {
+    Request public request;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        request = new Request();
 
         vm.stopBroadcast();
     }
